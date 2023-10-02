89be296cc9b0606c2919b1239ac9ed5c06716aaa
Julien THOMAS <thomasjulien92140@gmail.com>
2023-10-02 19:54:22 +0200

    Update node.py
    - min_gini init was inside the loop
    - Internal/Root classes are useless
    - input type fix for gini impurity

--- DecisionTree/node.py
+++ DecisionTree/node.py
@@ -4,40 +4,33 @@ from Dataset.dataset import Dataset
 
 
 class Node:
-    def __init__(self, subset: Dataset):
+    def __init__(self, subset: Dataset, true_child=None, false_child=None):
         self.subset = subset
         self.question = None
+        self.true_child = true_child
+        self.false_child = false_child
 
     def select_feature(self):
+        min_gini = float('inf')
         for feature in [col for col in self.subset.data.columns if col != self.subset.label]:
-            min_gini = float('inf')
             if self.subset.data[feature].dtype == 'bool':
                 candidate_question = Question(feature)
             else:
                 threshold = greedy_search(self.subset, feature)
                 candidate_question = Question(feature, threshold)
             true_set, false_set = candidate_question.answer(self.subset)
-            impurity = gini_impurity(self.subset, true_set, false_set)
+            impurity = gini_impurity(self.subset, true_set.data, false_set.data)
             if impurity < min_gini:
                 min_gini, self.question = impurity, candidate_question
 
-
-class Root(Node):
-    def __init__(self, subset: Dataset):
-        super().__init__(subset)
-        self.left_child = None
-        self.right_child = None
-
-
-class Internal(Node):
-    def __init__(self, subset: Dataset):
-        super().__init__(subset)
-        self.parent = None
-        self.left_child = None
-        self.right_child = None
+    def split_data(self):
+        true_set, false_set = self.question.answer(self.subset)
+        self.true_child, self.false_child = Node(true_set), Node(false_set)
 
 
 class Leaf(Node):
     def __init__(self, subset: Dataset):
         super().__init__(subset)
-        self.parent = None
+
+    def predict(self):
+        pass
